530dd2cb8d21c08fbbde95cad869ab19234a9d38
Matthew Capewell <matthewcapewell@googlemail.com>
2016-09-06 15:12:59 +0100

    Fixed blade layout, added topbar and added knockoutjs

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,46 @@
-var el = document.getElementsByClassName('coreui-splashscreen')[0];
-
-el.onclick = function() {
+document.getElementsByClassName('coreui-splashscreen')[0].onclick = function() {
     document.body.className += ' loaded';
 }
+
+document.getElementById('mySidenavButton').onclick = function() {
+    if (document.getElementById('mySidenav').hasClass('coreui-sidebar-is-collapsed'))
+        document.getElementById('mySidenav').removeClass('coreui-sidebar-is-collapsed');
+    else
+        document.getElementById('mySidenav').addClass('coreui-sidebar-is-collapsed');
+}
+
+Node.prototype.hasClass = function (className) {
+    if (this.classList) {
+        return this.classList.contains(className);
+    } else {
+        return (-1 < this.className.indexOf(className));
+    }
+};
+
+Node.prototype.addClass = function (className) {
+    if (this.classList) {
+        this.classList.add(className);
+    } else if (!this.hasClass(className)) {
+        var classes = this.className.split(" ");
+        classes.push(className);
+        this.className = classes.join(" ");
+    }
+    return this;
+};
+
+Node.prototype.removeClass = function (className) {
+    if (this.classList) {
+        this.classList.remove(className);
+    } else {
+        var classes = this.className.split(" ");
+        classes.splice(classes.indexOf(className), 1);
+        this.className = classes.join(" ");
+    }
+    return this;
+};
+
+var myViewModel = {
+    personName: 'Matthew Capewell',
+};
+
+ko.applyBindings(myViewModel);
